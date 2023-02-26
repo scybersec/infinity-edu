b01f83f60d99e44176824d4e65a22f5bf4641dcf
scybersec <81196168+scybersec@users.noreply.github.com>
2023-02-25 23:03:45 -0500

    fix

--- .next/static/7fNnZrXYDGkVmhVt21n57/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/566-2e9a3420273896f4.js","static/chunks/pages/index-3e38e254d4de64c5.js"],"/_error":["static/chunks/pages/_error-8e848193de3c139c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,13 @@ const Home = () => (
     <Navbar />
     <Hero />
     <div className="relative">
-      
       <div className="gradient-03 z-0" />
       <Explore />
     </div>
     <div className="relative">
       <GetStarted />
       <div className="gradient-04 z-0" />
+      <WhatsNew />
     </div>
     <World />
     <div className="relative">
