59bf11db254082b1fc426e0c8c642cb57a91fcfd
scybersec <81196168+scybersec@users.noreply.github.com>
2023-02-25 22:54:42 -0500

    new build

---
+++ .next/static/mpZ69pjcjVPS80fQXyqdF/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/566-2e9a3420273896f4.js","static/chunks/pages/index-e6a96e2e28114ca0.js"],"/_error":["static/chunks/pages/_error-8e848193de3c139c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
